5a01eb28e51e104981fdce58a30f7e32db1a1982
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-01-14 17:23:57 -0500

    Initial release

---
+++ buildRelease.bat
@@ -0,0 +1,55 @@
+@echo off
+set DEFHOMEDRIVE=d:
+set DEFHOMEDIR=%DEFHOMEDRIVE%%HOMEPATH%
+set HOMEDIR=
+set HOMEDRIVE=%CD:~0,2%
+
+set RELEASEDIR=d:\Users\jbb\release
+set ZIP="c:\Program Files\7-zip\7z.exe"
+echo Default homedir: %DEFHOMEDIR%
+
+rem set /p HOMEDIR= "Enter Home directory, or <CR> for default: "
+
+if "%HOMEDIR%" == "" (
+set HOMEDIR=%DEFHOMEDIR%
+)
+echo %HOMEDIR%
+
+SET _test=%HOMEDIR:~1,1%
+if "%_test%" == ":" (
+set HOMEDRIVE=%HOMEDIR:~0,2%
+)
+
+set VERSIONFILE=K2.version
+rem The following requires the JQ program, available here: https://stedolan.github.io/jq/download/
+c:\local\jq-win64  ".VERSION.MAJOR" %VERSIONFILE% >tmpfile
+set /P major=<tmpfile
+
+c:\local\jq-win64  ".VERSION.MINOR"  %VERSIONFILE% >tmpfile
+set /P minor=<tmpfile
+
+c:\local\jq-win64  ".VERSION.PATCH"  %VERSIONFILE% >tmpfile
+set /P patch=<tmpfile
+
+c:\local\jq-win64  ".VERSION.BUILD"  %VERSIONFILE% >tmpfile
+set /P build=<tmpfile
+del tmpfile
+set VERSION=%major%.%minor%.%patch%
+if "%build%" NEQ "0"  set VERSION=%VERSION%.%build%
+
+type K2.version
+
+echo Version:  %VERSION%
+
+rem set /p newVERSION= "Enter version: "
+rem if "%newVERSION" NEQ "" set VERSION=%newVERSION%
+
+copy ..\MiniAVC.dll GameData\JFJohnny5
+copy README.md GameData\JFJohnny5
+copy K2.version GameData\JFJohnny5
+copy LICENSE.txt GameData\JFJohnny5
+
+set FILE="%RELEASEDIR%\K2_Command_Pod-%VERSION%.zip"
+IF EXIST %FILE% del /F %FILE%
+%ZIP% a -tzip %FILE% GameData
+
